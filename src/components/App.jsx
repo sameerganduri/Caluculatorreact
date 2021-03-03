5bc9f2f239baa21ff153161f7c8ffc552aa88ebc
sameerganduri <42316233+sameerganduri@users.noreply.github.com>
2021-03-03 13:23:05 +0530

    made calci working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,35 @@
-import React, { Component } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./panelBody.css";
 
 function App() {
+  const [result, setResult] = useState("");
+  const inputRef = useRef(null);
+
+  useEffect(() => inputRef.current.focus());
+
+  function handleClick(e) {
+    setResult(result.concat(e.target.name));
+  }
+
+  function backspace() {
+    setResult(result.slice(0, -1));
+  }
+
+  function clear() {
+    setResult("");
+  }
+
+  function calculate() {
+    try {
+      setResult(eval(result).toString());
+    } catch (error) {
+      setResult("Error");
+    }
+  }
+
   return (
     <div className="App">
       <br />
@@ -15,52 +40,95 @@ function App() {
 
       <div className="container panelBody">
         <header>
-          {" "}
           <h1>Caluculator</h1>
         </header>
         <br />
-        <div className="Pad">
-          <input type="text" className="inputCss" />
-        </div>
+        <from>
+          <input
+            className="inputCss"
+            type="text"
+            value={result}
+            ref={inputRef}
+          />
+        </from>
         <br />
-        <div className="Pad">
-          <input type="text" className="resultCss" readOnly />
-        </div>
-        <br />
 
         <div className="rowPad">
-          <button className="btn btn-dark">1</button>
-          <button className="btn btn-dark">2</button>
-          <button className="btn btn-dark">3</button>
-          <button className="btn btn-primary">+</button>
-          <button className="btn btn-primary">(</button>
-        </div>
-
-        <div className="rowPad">
-          <button className="btn btn-dark">4</button>
-          <button className="btn btn-dark">5</button>
-          <button className="btn btn-dark">6</button>
-          <button className="btn btn-primary">-</button>
-          <button className="btn btn-primary">)</button>
+          <button className="btn btn-dark" name="1" onClick={handleClick}>
+            1
+          </button>
+          <button className="btn btn-dark" name="2" onClick={handleClick}>
+            2
+          </button>
+          <button className="btn btn-dark" name="3" onClick={handleClick}>
+            3
+          </button>
+          <button className="btn btn-primary" name="+" onClick={handleClick}>
+            +
+          </button>
+          <button className="btn btn-primary" name="(" onClick={handleClick}>
+            (
+          </button>
         </div>
 
         <div className="rowPad">
-          <button className="btn btn-dark">7</button>
-          <button className="btn btn-dark">8</button>
-          <button className="btn btn-dark">9</button>
-          <button className="btn btn-primary">*</button>
-          <button className="btn btn-primary">%</button>
+          <button className="btn btn-dark" name="4" onClick={handleClick}>
+            4
+          </button>
+          <button className="btn btn-dark" name="5" onClick={handleClick}>
+            5
+          </button>
+          <button className="btn btn-dark" name="6" onClick={handleClick}>
+            6
+          </button>
+          <button className="btn btn-primary" name="-" onClick={handleClick}>
+            -
+          </button>
+          <button className="btn btn-primary" name=")" onClick={handleClick}>
+            )
+          </button>
         </div>
 
         <div className="rowPad">
-          <button className="btn btn-dark">0</button>
-          <button className="btn btn-dark">.</button>
-          <button className="btn btn-primary">^</button>
+          <button className="btn btn-dark" name="7" onClick={handleClick}>
+            7
+          </button>
+          <button className="btn btn-dark" name="8" onClick={handleClick}>
+            8
+          </button>
+          <button className="btn btn-dark" name="9" onClick={handleClick}>
+            9
+          </button>
+          <button className="btn btn-primary" name="*" onClick={handleClick}>
+            *
+          </button>
+          <button className="btn btn-primary" name="%" onClick={handleClick}>
+            %
+          </button>
         </div>
 
         <div className="rowPad">
-          <button className="btn btn-info">result</button>
-          <button className="btn btn-info">clearall</button>
+          <button className="btn btn-dark" name="0" onClick={handleClick}>
+            0
+          </button>
+          <button className="btn btn-dark" name="." onClick={handleClick}>
+            .
+          </button>
+          <button className="btn btn-primary" name="^" onClick={handleClick}>
+            ^
+          </button>
+          <button className="btn btn-info" id="backspace" onClick={backspace}>
+            C
+          </button>
+        </div>
+
+        <div className="rowPad">
+          <button className="btn btn-info" id="result" onClick={calculate}>
+            result
+          </button>
+          <button className="btn btn-info" id="clearall" onClick={clear}>
+            clearall
+          </button>
         </div>
       </div>
     </div>
